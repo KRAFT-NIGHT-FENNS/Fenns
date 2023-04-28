2a173e7e73ba577b17ece265b9022f59a60e4f74
sreelalv <sreelalv.sr@gmail.com>
2023-04-29 05:11:41 +0530

    Home page design
    methods to fetch current location of the device

--- lib/amb.dart
+++ lib/amb.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:geolocator/geolocator.dart';
 
-class AmbulancePage extends StatelessWidget {
+class AmbulancePage extends StatefulWidget {
   const AmbulancePage({Key? key}) : super(key: key);
+
+  @override
+  _AmbulancePageState createState() => _AmbulancePageState();
+}
+
+class _AmbulancePageState extends State<AmbulancePage> {
+  final TextEditingController _locationController = TextEditingController();
+
+  @override
+  void dispose() {
+    _locationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,20 +24,74 @@ class AmbulancePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Emergency Services'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Call 911'),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.8),
+          ),
+          child: FractionallySizedBox(
+            widthFactor: 0.5,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FractionallySizedBox(
+                  widthFactor: 1.0,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Call 911'),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                FractionallySizedBox(
+                  widthFactor: 1.0,
+                  child: ElevatedButton(
+                    onPressed: () => _alertAmbulance(context),
+                    child: const Text('Alert Ambulance'),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Stack(
+                  children: [
+                    TextFormField(
+                      controller: _locationController,
+                      readOnly: true,
+                      decoration: InputDecoration(
+                        labelText: 'Current Location',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    Positioned(
+                      right: 0,
+                      top: 0,
+                      bottom: 0,
+                      child: GestureDetector(
+                        onTap: () => _fetchLocation(context),
+                        child: Container(
+                          padding: const EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: const Icon(
+                            Icons.my_location,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => _alertAmbulance(context),
-              child: const Text('Alert Ambulance'),
-            ),
-          ],
+          ),
         ),
       ),
     );
@@ -49,5 +117,20 @@ class AmbulancePage extends StatelessWidget {
         );
       },
     );
+  }
+
+  void _fetchLocation(BuildContext context) async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      String location =
+          'Latitude: ${position.latitude}, Longitude: ${position.longitude}';
+      setState(() {
+        _locationController.text = location;
+      });
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Could not fetch location. Error: $e')));
+    }
   }
 }
